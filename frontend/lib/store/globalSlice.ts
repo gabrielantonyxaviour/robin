537e18c89168c86df1d212f087a268a9a423170c
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-12-03 19:41:51 +0530

    slice clean up

--- frontend/lib/store/globalSlice.ts
+++ frontend/lib/store/globalSlice.ts
@@ -1,27 +1,11 @@
 import { StateCreator } from "zustand";
-import { createKintoSDK, KintoAccountInfo } from "kinto-web-sdk";
-import { KYCViewerInfo } from "../type";
-interface GlobalState {
-  kintoSdk: any;
-  accountInfo: KintoAccountInfo | null;
-  kycViewerInfo: KYCViewerInfo | null;
-  overallDonations: number;
-}
+interface GlobalState {}
 
-interface GlobalActions {
-  setAccountInfo: (accountInfo: KintoAccountInfo) => void;
-  setOverallDonations: (overallDonations: number) => void;
-  setKYCViewerInfo: (kycViewerInfo: KYCViewerInfo) => void;
-}
+interface GlobalActions {}
 
 export type GlobalSlice = GlobalState & GlobalActions;
 
-export const initialGlobalState: GlobalState = {
-  kintoSdk: createKintoSDK("0xA112c44A6E4DB0E00c58091c6dE1121e49f83Eec"),
-  accountInfo: null,
-  kycViewerInfo: null,
-  overallDonations: 0,
-};
+export const initialGlobalState: GlobalState = {};
 
 export const createGlobalSlice: StateCreator<
   GlobalSlice,
@@ -30,7 +14,4 @@ export const createGlobalSlice: StateCreator<
   GlobalSlice
 > = (set) => ({
   ...initialGlobalState,
-  setAccountInfo: (accountInfo) => set({ accountInfo }),
-  setKYCViewerInfo: (kycViewerInfo) => set({ kycViewerInfo }),
-  setOverallDonations: (overallDonations) => set({ overallDonations }),
 });
