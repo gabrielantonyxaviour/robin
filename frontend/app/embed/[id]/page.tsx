d7f14f7aa471402cb0b7f4f193a4f726a761521e
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2025-01-07 02:21:34 +0530

    project metadata

--- frontend/app/embed/[id]/page.tsx
+++ frontend/app/embed/[id]/page.tsx
@@ -1,7 +1,44 @@
-export const metadata = {
-  title: "Embedded Content",
-};
+import type { Metadata } from "next";
 
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  return {
+    title: "RobinX | Gamified Quests",
+    description:
+      "An autonomous AI agent that teaches web3 to users in the form of gamified quests.",
+    openGraph: {
+      title: "RobinX | Gamified Quests",
+      description:
+        "An autonomous AI agent that teaches web3 to users in the form of gamified quests.",
+      images: ["/robin.jpg"],
+    },
+    other: {
+      "twitter:player": `https://robinx-ai.vercel.app/embed/${params.id}`,
+      "x-frame-options": "ALLOWALL",
+      "content-security-policy":
+        "frame-ancestors 'self' https://twitter.com https://x.com;",
+    },
+    twitter: {
+      card: "player",
+      site: "https://x.com/EduRobinX",
+      title: "ZoroX | World's Best Memecoin Hunter",
+      images: ["https://zorox-ai.vercel.app/robin.jpg"],
+      description:
+        "An autonomous AI agent that teaches web3 to users in the form of gamified quests.",
+      players: [
+        {
+          playerUrl: `https://robinx-ai.vercel.app/embed/${params.id}`,
+          streamUrl: `https://robinx-ai.vercel.app/embed/${params.id}`,
+          width: 360,
+          height: 560,
+        },
+      ],
+    },
+  };
+}
 export default function EmbedPage({ params }: { params: { id: string } }) {
   return (
     <div style={{ width: "100%", height: "100%", backgroundColor: "#000" }}>
