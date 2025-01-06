30527371e32608e41d529be0fac550a46ca0b1d8
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2025-01-07 02:11:45 +0530

    worldcoin cleanup

--- frontend/components/home/world.tsx
+++ frontend/components/home/world.tsx
@@ -14,85 +14,84 @@ import { decodeAbiParameters, parseAbiParameters } from "viem";
 import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import Image from "next/image";
-import { ROBINX_VERIFIER_ABI, ROBINX_VERIFIER_ADDRESS } from "@/lib/constants";
 import { useToast } from "@/hooks/use-toast";
 import { ToastAction } from "../ui/toast";
 import { sepolia } from "viem/chains";
 import { shortenAddress, shortenNullifier } from "@/lib/utils";
 
 export default function World({ close }: { close: () => void }) {
-  const { address, chainId } = useAccount();
-  const { switchChainAsync } = useSwitchChain();
-  const { toast } = useToast();
-  const { setOpen } = useIDKit();
-  const [done, setDone] = useState(false);
-  const {
-    data: hash,
-    isPending,
-    error,
-    writeContractAsync,
-  } = useWriteContract();
-  const [nullifier, setNullifier] = useState(0);
+  // const { address, chainId } = useAccount();
+  // const { switchChainAsync } = useSwitchChain();
+  // const { toast } = useToast();
+  // const { setOpen } = useIDKit();
+  // const [done, setDone] = useState(false);
+  // const {
+  //   data: hash,
+  //   isPending,
+  //   error,
+  //   writeContractAsync,
+  // } = useWriteContract();
+  // const [nullifier, setNullifier] = useState(0);
 
-  const submitTx = async (proof: ISuccessResult) => {
-    try {
-      if (chainId != sepolia.id) {
-        await switchChainAsync({
-          chainId: sepolia.id,
-        });
-      }
-      await writeContractAsync({
-        address: ROBINX_VERIFIER_ADDRESS as `0x${string}`,
-        account: address,
-        abi: ROBINX_VERIFIER_ABI,
-        functionName: "verifyAndExecute",
-        args: [
-          address as `0x${string}`,
-          BigInt(proof.merkle_root),
-          BigInt(proof.nullifier_hash),
-          decodeAbiParameters(
-            parseAbiParameters("uint256[8]"),
-            proof.proof as `0x${string}`
-          )[0],
-        ],
-      });
-    } catch (error) {
-      throw new Error((error as BaseError).shortMessage);
-    }
-  };
+  // const submitTx = async (proof: ISuccessResult) => {
+  //   try {
+  //     if (chainId != sepolia.id) {
+  //       await switchChainAsync({
+  //         chainId: sepolia.id,
+  //       });
+  //     }
+  //     await writeContractAsync({
+  //       address: ROBINX_VERIFIER_ADDRESS as `0x${string}`,
+  //       account: address,
+  //       abi: ROBINX_VERIFIER_ABI,
+  //       functionName: "verifyAndExecute",
+  //       args: [
+  //         address as `0x${string}`,
+  //         BigInt(proof.merkle_root),
+  //         BigInt(proof.nullifier_hash),
+  //         decodeAbiParameters(
+  //           parseAbiParameters("uint256[8]"),
+  //           proof.proof as `0x${string}`
+  //         )[0],
+  //       ],
+  //     });
+  //   } catch (error) {
+  //     throw new Error((error as BaseError).shortMessage);
+  //   }
+  // };
 
-  useEffect(() => {
-    if (hash) {
-      toast({
-        title: "Transcaction Success",
-        description: "Your WorldId has been verified on Sepolia.",
-        action: (
-          <ToastAction
-            className="bg-[#e7ccfc] hover:bg-[#e7ccfc] text-black hover:text-black border-[2px] border-black mr-[2px] rounded-sm"
-            altText="view tx"
-            onClick={() => {
-              window.open(
-                "hhttps://eth-sepolia.blockscout.com/tx/" + hash,
-                "_blank"
-              );
-            }}
-          >
-            View Tx
-          </ToastAction>
-        ),
-      });
-    }
-  }, [hash]);
+  // useEffect(() => {
+  //   if (hash) {
+  //     toast({
+  //       title: "Transcaction Success",
+  //       description: "Your WorldId has been verified on Sepolia.",
+  //       action: (
+  //         <ToastAction
+  //           className="bg-[#e7ccfc] hover:bg-[#e7ccfc] text-black hover:text-black border-[2px] border-black mr-[2px] rounded-sm"
+  //           altText="view tx"
+  //           onClick={() => {
+  //             window.open(
+  //               "hhttps://eth-sepolia.blockscout.com/tx/" + hash,
+  //               "_blank"
+  //             );
+  //           }}
+  //         >
+  //           View Tx
+  //         </ToastAction>
+  //       ),
+  //     });
+  //   }
+  // }, [hash]);
   return (
     <div className="w-[400px] h-[240px] absolute top-[32%] left-[38%] bg-black rounded-sm">
-      <IDKitWidget
+      {/* <IDKitWidget
         app_id={process.env.WORLDCOIN_APP_ID as `app_${string}`}
         action={process.env.WORLDCOIN_ACTION as string}
         signal={address}
         onSuccess={submitTx}
         autoClose
-      />
-      <div
+      /> */}
+      {/* <div
         onClick={() => {}}
         className={`absolute w-[400px] h-[240px] flex flex-col items-center -top-[1%] -left-[1%] w-full h-full space-y-2 sen rounded-sm text-sm border border-[2px] border-black py-2 bg-[#ffd75f] text-black`}
       >
@@ -138,7 +137,7 @@ export default function World({ close }: { close: () => void }) {
             </Button>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
