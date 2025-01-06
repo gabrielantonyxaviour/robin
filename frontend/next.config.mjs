8425c82f08583564683a725bee4a013e91943a12
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2025-01-07 02:33:20 +0530

    next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/embed/:id*", // Handle /embed and /embed/[id]
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *;",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
