b76b3c446d1312d2028ccf189320b79f70f3c0b7
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2025-01-08 00:48:04 +0530

    pdo

--- frontend/app/embed/[id]/page.tsx
+++ frontend/app/embed/[id]/page.tsx
@@ -1,43 +1,54 @@
 import type { Metadata } from "next";
+import { useSearchParams } from "next/navigation";
 
-export const metadata: Metadata = {
-  title: "RobinX | Gamified Quests",
-  description:
-    "An autonomous AI agent that teaches web3 to users in the form of gamified quests.",
-  openGraph: {
+export async function generateMetadata({
+  searchParams,
+}: {
+  searchParams: { id: string };
+}): Promise<Metadata> {
+  const { id } = searchParams;
+  return {
     title: "RobinX | Gamified Quests",
     description:
       "An autonomous AI agent that teaches web3 to users in the form of gamified quests.",
-    images: ["https://robinx-ai.vercel.app/robin.jpg"],
-  },
-  other: {
-    "twitter:player": `https://robinx-ai.vercel.app/embed/0x1`,
-    "x-frame-options": "ALLOWALL",
-    "content-security-policy":
-      "frame-ancestors 'self' https://twitter.com https://x.com;",
-  },
-  twitter: {
-    card: "player",
-    site: "https://x.com/EduRobinX",
-    title: "RobinX | Gamified Quests",
-    images: ["https://robinx-ai.vercel.app/robin.jpg"],
-    description:
-      "An autonomous AI agent that teaches web3 to users in the form of gamified quests.",
-    players: [
-      {
-        playerUrl: `https://robinx-ai.vercel.app/embed/0x1`,
-        streamUrl: `https://robinx-ai.vercel.app/embed/0x1`,
-        width: 360,
-        height: 560,
-      },
-    ],
-  },
-};
+    openGraph: {
+      title: "RobinX | Gamified Quests",
+      description:
+        "An autonomous AI agent that teaches web3 to users in the form of gamified quests.",
+      images: ["https://robinx-ai.vercel.app/robin.jpg"],
+    },
+    other: {
+      "twitter:player": `https://robinx-ai.vercel.app/embed/${id}`,
+      "x-frame-options": "ALLOWALL",
+      "content-security-policy":
+        "frame-ancestors 'self' https://twitter.com https://x.com;",
+    },
+    twitter: {
+      card: "player",
+      site: "https://x.com/EduRobinX",
+      title: "RobinX | Gamified Quests",
+      images: ["https://robinx-ai.vercel.app/robin.jpg"],
+      description:
+        "An autonomous AI agent that teaches web3 to users in the form of gamified quests.",
+      players: [
+        {
+          playerUrl: `https://robinx-ai.vercel.app/embed/${id}`,
+          streamUrl: `https://robinx-ai.vercel.app/embed/${id}`,
+          width: 360,
+          height: 560,
+        },
+      ],
+    },
+  };
+}
 export default function EmbedPage({ params }: { params: { id: string } }) {
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
+
   return (
     <div style={{ width: "100%", height: "100%", backgroundColor: "#000" }}>
       <iframe
-        src={`https://robinx-ai.vercel.app/quiz/0x1`}
+        src={`https://robinx-ai.vercel.app/quiz/${id}`}
         style={{
           width: "100%",
           height: "100%",
