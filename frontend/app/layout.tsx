52ef4262c3fd60d77978e3b9b52d5fe41319d945
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2025-01-08 00:36:30 +0530

    ads

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -6,12 +6,14 @@ import { EnvironmentStoreProvider } from "@/components/context";
 import Layout from "@/components/layout";
 import OCConnectProvider from "@/components/providers/occonnect-provider";
 import WalletProvider from "@/components/providers/wallet-provider";
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
   return (
     <EnvironmentStoreProvider>
       <html lang="en">
